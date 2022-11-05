99a5515d0335d56597568bf2557a5aa2dc38b02a
Mustafa UYGUR <mustafauygur@outlook.com>
2022-11-06 02:22:31 +0300

    (FIX) REMOVE HAVE BEEN MAGIC STRINGS

--- src/main/java/dev/uygur/constants/Messages.java
+++ src/main/java/dev/uygur/constants/Messages.java
@@ -5,6 +5,7 @@ public class Messages {
     public static final String EMAIL = "mustafauygurr@gmail.com";
     public static final String PASSWORD = "123456789abcd";
 
+
     public static final String CREDIT_CARD_OWNER = "Mustafa Uygur";
     public static final String CREDIT_CARD_FIRST_NUMBER = "4912";
     public static final String CREDIT_CARD_SECOND_NUMBER = "1235";
@@ -14,14 +15,19 @@ public class Messages {
     public static final String CREDIT_CARD_DATE_MONTH = "11";
     public static final String CREDIT_CARD_DATE_YEAR = "2023";
 
+
     public static final String FIRST_NAME = "Mustafa";
     public static final String LAST_NAME = "Uygur";
     public static final String CITY = "İstanbul";
     public static final String ZONE = "EYÜPSULTAN";
     public static final String ADDRESS = "ORDU CAD. No:125 Kat:1 Daire:1";
     public static final String DISTRICT = "YENİDOĞAN MAH";
-    public static final String ZIP_CODE = "Y34245";
+    public static final String ZIP_CODE = "34245";
     public static final String PHONE_NUMBER = "5334678290";
     public static final String NATIONAL_IDENTITY = "52168107718";
+
+    
     public static final String SELECT_POINT_CATALOG_HIGH_RATING = "Yüksek Oylama";
+    public static final String SEARCH_TEXT = "Oyuncak";
+    public static final String FAVORITE_CONTROL_TEXT = "favorilerinize";
 }
--- src/main/java/dev/uygur/pages/ProductPage.java
+++ src/main/java/dev/uygur/pages/ProductPage.java
@@ -1,8 +1,10 @@
 package dev.uygur.pages;
 
+import dev.uygur.constants.Messages;
 import dev.uygur.methods.Methods;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.Message;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 
@@ -24,26 +26,26 @@ public class ProductPage {
         goToBackHomePage();
     }
 
-    private void searchToText(){
-        methods.sendKeys(By.id("search-input"), "oyuncak");
+    private void searchToText() {
+        methods.sendKeys(By.id("search-input"), Messages.SEARCH_TEXT);
         methods.waitBySecond(5);
 
         methods.click(By.cssSelector(".button-search"));
         methods.waitBySecond(3);
     }
 
-    private void scrollThePage(){
+    private void scrollThePage() {
         methods.scrollWithAction(By.xpath("//div[@class=\"product-list\"]/div[7]"));
         methods.waitBySecond(5);
     }
 
-    private void addProductToFavorite(){
+    private void addProductToFavorite() {
         methods.click(By.xpath("//div[@class=\"product-list\"]/div[6]/div[@class=\"grid_2 alpha omega relative\"]/div[@class=\"hover-menu\"]/a[@class=\"add-to-favorites\"]"));
         methods.clickWithJavascript("onclick");
         methods.waitBySecond(5);
 
         String value6 = methods.getText(By.cssSelector(".swal2-title.ky-swal-title-single>a"));
-        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.","favorilerinize",value6);
+        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.", Messages.FAVORITE_CONTROL_TEXT, value6);
         methods.waitBySecond(2);
 
         methods.click(By.xpath("//div[@class=\"product-list\"]/div[5]/div[@class=\"grid_2 alpha omega relative\"]/div[@class=\"hover-menu\"]/a[@class=\"add-to-favorites\"]"));
@@ -51,7 +53,7 @@ public class ProductPage {
         methods.waitBySecond(3);
 
         String value5 = methods.getText(By.cssSelector(".swal2-title.ky-swal-title-single>a"));
-        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.","favorilerinize",value5);
+        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.", Messages.FAVORITE_CONTROL_TEXT, value5);
         methods.waitBySecond(2);
 
         methods.click(By.xpath("//div[@class=\"product-list\"]/div[4]/div[@class=\"grid_2 alpha omega relative\"]/div[@class=\"hover-menu\"]/a[@class=\"add-to-favorites\"]"));
@@ -59,7 +61,7 @@ public class ProductPage {
         methods.waitBySecond(3);
 
         String value4 = methods.getText(By.cssSelector(".swal2-title.ky-swal-title-single>a"));
-        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.","favorilerinize",value4);
+        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.", Messages.FAVORITE_CONTROL_TEXT, value4);
         methods.waitBySecond(2);
 
         methods.click(By.xpath("//div[@class=\"product-list\"]/div[8]/div[@class=\"grid_2 alpha omega relative\"]/div[@class=\"hover-menu\"]/a[@class=\"add-to-favorites\"]"));
@@ -67,11 +69,11 @@ public class ProductPage {
         methods.waitBySecond(3);
 
         String value8 = methods.getText(By.cssSelector(".swal2-title.ky-swal-title-single>a"));
-        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.","favorilerinize",value8);
+        Assert.assertEquals("Favorilere ekleme işlemi yapılamadı.", Messages.FAVORITE_CONTROL_TEXT, value8);
         methods.waitBySecond(2);
     }
 
-    private void goToBackHomePage(){
+    private void goToBackHomePage() {
 
         methods.click(By.cssSelector(".logo-text>a"));
         methods.waitBySecond(3);
